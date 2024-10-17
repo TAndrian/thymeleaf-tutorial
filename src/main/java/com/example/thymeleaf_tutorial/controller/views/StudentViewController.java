eb7f6f1367ed4d2b55c8bd7fb700faf333c85744
TAndrian <LenOkimaru@gmail.com>
2024-10-17 12:07:29 +0200

    tutorial(condition-expression) : fix DTO to enable gender value comparison in view

--- src/main/java/com/example/thymeleaf_tutorial/controller/views/StudentViewController.java
+++ src/main/java/com/example/thymeleaf_tutorial/controller/views/StudentViewController.java
@@ -1,7 +1,6 @@
 package com.example.thymeleaf_tutorial.controller.views;
 
 import com.example.thymeleaf_tutorial.dto.StudentDTO;
-import com.example.thymeleaf_tutorial.model.enums.Gender;
 import com.example.thymeleaf_tutorial.service.StudentService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -21,12 +20,15 @@ public class StudentViewController {
 
     @GetMapping("/student")
     public String studentTemplate(Model model) {
-        StudentDTO student = new StudentDTO(UUID.fromString("e6d1fbb3-6b5a-4fbd-bb2f-2a7f5bc4cdb1"), "Alice", "Smith", Gender.FEMALE);
+        StudentDTO student = new StudentDTO(
+                UUID.fromString("e6d1fbb3-6b5a-4fbd-bb2f-2a7f5bc4cdb1"),
+                "Alice",
+                "Smith",
+                "FEMALE");
         List<StudentDTO> students = studentService.getStudents();
 
         model.addAttribute("student", student);
         model.addAttribute("students", students);
         return "student-view";
     }
-
 }
--- src/main/java/com/example/thymeleaf_tutorial/dto/StudentDTO.java
+++ src/main/java/com/example/thymeleaf_tutorial/dto/StudentDTO.java
@@ -1,13 +1,11 @@
 package com.example.thymeleaf_tutorial.dto;
 
-import com.example.thymeleaf_tutorial.model.enums.Gender;
-
 import java.util.UUID;
 
 public record StudentDTO(
         UUID id,
         String firstname,
         String lastname,
-        Gender gender
+        String gender
 ) {
 }
